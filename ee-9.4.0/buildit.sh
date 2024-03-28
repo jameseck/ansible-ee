2fdd55ae525090093e57c306fa3498a37a00428d
James Eckersall <james.eckersall@gmail.com>
2024-03-28 08:22:33 +0000

    added ansible-9.4.0 image

---
+++ ee-9.4.0/buildit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+EE_ANSIBLE_VERSION=9.4.0
+
+ansible-builder build \
+  -v 3 \
+  -t test-ee:$EE_ANSIBLE_VERSION
+
+ansible-navigator --pull-policy never \
+    --execution-environment-image test-ee:$EE_ANSIBLE_VERSION \
+    -m stdout \
+    run tests.yml
